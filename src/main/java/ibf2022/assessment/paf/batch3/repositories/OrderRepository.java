cfac37a39b7965751a20824354544ddd33f4e95c
mich <mich.cxy@gmail.com>
2023-04-28 17:21:07 +0800

    forgot the database folder

--- src/main/java/ibf2022/assessment/paf/batch3/repositories/OrderRepository.java
+++ src/main/java/ibf2022/assessment/paf/batch3/repositories/OrderRepository.java
@@ -2,7 +2,9 @@ package ibf2022.assessment.paf.batch3.repositories;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public class OrderRepository {
 
 	@Autowired
