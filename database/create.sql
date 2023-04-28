7df9a4324c67c345934d4eca71e6634e4fff6680
mich <mich.cxy@gmail.com>
2023-04-28 17:17:40 +0800

    forgot the database folder

---
+++ database/create.sql
@@ -0,0 +1,16 @@
+-- Task 1
+-- Write SQL statements in this file to create the brewery database and 
+-- populate the database with the given SQL files
+
+-- mysql -u root -p brewery < beers.sql
+-- mysql -u root -p brewery < breweries.sql
+-- mysql -u root -p brewery < categories.sql
+-- mysql -u root -p brewery < geocodes.sql
+-- mysql -u root -p brewery < styles.sql
+
+Source C:/Users/mich/ibf/assessment_template/database/beers.sql
+Source C:/Users/mich/ibf/assessment_template/database/breweries.sql
+Source C:/Users/mich/ibf/assessment_template/database/categories.sql
+Source C:/Users/mich/ibf/assessment_template/database/geocodes.sql
+Source C:/Users/mich/ibf/assessment_template/database/styles.sql
+
